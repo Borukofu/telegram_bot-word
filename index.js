c6dfebe12ad788e3a2ce9154727b0944e82f9810
Borukofu <Borukofu.ua@gmail.com>
2023-05-30 16:42:37 +0300

    update

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ require('dotenv').config()
 
 const {TOKEN} = process.env
 
+let matches_text,matches_digit
 
 bot = new Telegraf(TOKEN)
 bot.start((ctx) => ctx.reply('Добро пожаливать в бота которий даёш мне букви я из них делаю слова (и длину слова из этих букв например(ток 3))'));
-bot.on('text', (ctx) =>{
-
+bot.on('text',async (ctx) =>{
+    try{
+        texts = ctx.message.text
+        matches_text = await texts.match(/\p{N}/gu);
+        matches_digit = await texts.match(/\p{L}/gu)
+        console.log(matches_text.join(""))
+        console.log(matches_digit.join(""))
+        if (matches_text!=null && matches_digit!=null) {
+            console.log("!=")
+    }}catch(err){
+        console.error(err)
+    }
 });
 bot.launch();
 

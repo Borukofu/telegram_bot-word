f4bd23f008b0a0fd2d7af8a9c525314178f62771
Borukofu <Borukofu.ua@gmail.com>
2023-05-30 17:28:12 +0300

    update

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 console.log("starded...")
 const {Telegraf} = require("telegraf")
+const itertools = require('itertools')
+const words = require('./words_Russian')
 require('dotenv').config()
-
 const {TOKEN} = process.env
-
 let matches_text,matches_digit
+
+function search(chars, length) {
+  const letters = chars.split('');
+  const combinations = itertools.permutations(letters, length);
+  const foundWords = [];
+
+  for (const combination of combinations) {
+    const word = combination.join('');
+    if (words.includes(word)) {
+      foundWords.push(word)
+    }
+  }
+
+  if (foundWords.length > 0) {
+    return String(foundWords);
+  } else {
+    return "not";
+  }
+}
+//console.log(search("ток",3))
 
 bot = new Telegraf(TOKEN)
 bot.start((ctx) => ctx.reply('Добро пожаливать в бота которий даёш мне букви я из них делаю слова (и длину слова из этих букв например(ток 3))'));
 bot.on('text',async (ctx) =>{
     try{
         texts = ctx.message.text
-        matches_text = await texts.match(/\p{N}/gu);
-        matches_digit = await texts.match(/\p{L}/gu)
-        console.log(matches_text.join(""))
-        console.log(matches_digit.join(""))
+        matches_text = await texts.match(/\p{L}/gu);
+        matches_digit = await texts.match(/\p{N}/gu);
+        console.log(matches_text.join(""),matches_digit.join(""))
         if (matches_text!=null && matches_digit!=null) {
-            console.log("!=")
+            ctx.reply(search(matches_text.join(""),matches_digit.join("")))
     }}catch(err){
         console.error(err)
     }
